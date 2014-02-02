29026b7288304b36a45d81e568c6972e24d8f459
Brady Vercher <brady@blazersix.com>
2014-02-01 17:12:39 -0800

    Fix util.slugify().

--- tasks/lib/wordpress.js
+++ tasks/lib/wordpress.js
@@ -95,15 +95,15 @@ exports.init = function( grunt ) {
 	 */
 	exports.slugify = function() {
 		var slug = path.basename( process.cwd() ),
-			slug2 = path.basename( path.basename( process.cwd() ) );
+			slug2 = path.basename( path.dirname( process.cwd() ) );
 
 		if ( 'trunk' === slug ) {
 			slug = slug2;
 		} else if ( -1 !== [ 'branches', 'tags' ].indexOf( slug2 ) ) {
-			slug = path.basename( path.basename( path.basename( process.cwd() ) ) );
+			slug = path.basename( path.dirname( path.dirname( process.cwd() ) ) );
 		}
 
-		return path.basename( process.cwd() );
+		return slug;
 	};
 
 	return exports;
