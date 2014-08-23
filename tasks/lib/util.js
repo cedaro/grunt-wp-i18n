1f50e6e7cdd66718e9e838db0032c910959734bb
Brady Vercher <brady@blazersix.com>
2014-08-22 22:05:07 -0700

    Introduce common Poedit headers. Related #28

--- tasks/lib/util.js
+++ tasks/lib/util.js
@@ -91,6 +91,30 @@ exports.init = function( grunt ) {
 			return pot;
 		}
 
+		var poedit = {
+			'language': 'en',
+			'plural-forms': 'nplurals=2; plural=(n != 1);',
+			'x-poedit-country': 'United States',
+			'x-poedit-sourcecharset': 'UTF-8',
+			'x-poedit-keywordslist': true,
+			'x-poedit-basepath': '../',
+			'x-poedit-searchpath-0': '.',
+			'x-poedit-bookmarks': '',
+			'x-textdomain-support': 'yes'
+		};
+
+		// Add default Poedit headers.
+		if ( 'poedit' in headers && true === headers['poedit'] ) {
+			headers = _.extend( poedit, headers );
+			delete headers['poedit'];
+		}
+
+		// Add the the Poedit keywordslist header.
+		if ( 'x-poedit-keywordslist' in headers && true === headers['x-poedit-keywordslist'] ) {
+			pot.headers['x-poedit-keywordslist'] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;';
+			delete headers['x-poedit-keywordslist'];
+		}
+
 		// Merge headers
 		headers = _.extend( pot.headers, headers );
 
